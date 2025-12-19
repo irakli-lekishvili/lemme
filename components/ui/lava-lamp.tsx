b6d88dbc88f2cc80ac65cef5a82ad82dd9ad3744
Irakli Lekishvili <l3kishvili@gmail.com>
2025-12-19 15:30:06 +0400

    Replace lava lamp background with a mesh grid design, updating styles and structure in the CSS and component files.

--- components/ui/lava-lamp.tsx
+++ components/ui/lava-lamp.tsx
@@ -1,12 +1,7 @@
 export function LavaLamp() {
   return (
-    <div className="lava-lamp">
-      <div className="lava-blob lava-blob-1" />
-      <div className="lava-blob lava-blob-2" />
-      <div className="lava-blob lava-blob-3" />
-      <div className="lava-blob lava-blob-4" />
-      <div className="lava-blob lava-blob-5" />
-      <div className="lava-blob lava-blob-6" />
+    <div className="mesh-grid">
+      <div className="mesh-grid-lines" />
     </div>
   );
 }
